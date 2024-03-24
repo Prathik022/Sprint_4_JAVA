1d82ace6ae72cf97d96c7f0e1b70ae70e9a7f230
prr <prr@ESKW230578>
2024-03-24 10:12:33 +0530

    Adding new springboot application

---
+++ Sp4_SpringBoot_Application/src/main/java/com/esko/Sp4_SpringBoot_Application/Sp4SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.esko.Sp4_SpringBoot_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp4SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp4SpringBootApplication.class, args);
+	}
+}
---
+++ Sp4_SpringBoot_Application/src/test/java/com/esko/Sp4_SpringBoot_Application/Sp4SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esko.Sp4_SpringBoot_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp4SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
